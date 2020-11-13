fe8d83c3bf6c7b477d978ddda5631c1d6cb19a84
bejeri4 <lkara15@freeuni.edu.ge>
2020-11-13 21:54:18 +0400

    Create FindDuplicateInArray.py

---
+++ FindDuplicateInArray.py
@@ -0,0 +1,25 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def repeatedNumber(self, A):
+        matcher = [0] * (int(math.log(len(A) + 1, 2)) + 1)
+        for elem in A:
+            log = int(math.log(elem, 2))
+            matcher[log] += 1
+        mismatchRange = -1
+        for i in range(len(matcher)):
+            if 2**i != matcher[i]:
+                mismatchRange = i
+                break
+        if mismatchRange == -1:
+            return -1
+        delta = 2**mismatchRange
+        arr = [0] * delta
+        for elem in A:
+            log = int(math.log(elem, 2))
+            if log == mismatchRange:
+                arr[(elem - delta)] += 1
+        for i in range(len(arr)):
+            if arr[i] != 1:
+                return i + delta
+        return mismatchRange
