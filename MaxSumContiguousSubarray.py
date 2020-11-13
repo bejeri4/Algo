f69af0cff14f5110d07d7cb3403ec8acbf3f2c51
bejeri4 <lkara15@freeuni.edu.ge>
2020-11-13 11:22:38 +0400

    Create MaxSumContiguousSubarray.py

---
+++ MaxSumContiguousSubarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxSubArray(self, A):
+        results = []
+        curSum = 0
+        for elem in A:
+            curSum += elem
+            results.append(curSum)
+            if curSum < 0:
+                curSum = 0
+        max = -math.inf
+        for elem in results:
+            if elem > max:
+                max = elem
+        return max
