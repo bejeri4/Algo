941f4f849aa443093b8ae843d624080451f2ae2c
bejeri4 <lkara15@freeuni.edu.ge>
2020-11-16 15:19:00 +0400

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -3,12 +3,15 @@ def swap(arr, i, j):
     arr[i] = arr[j]
     arr[j] = tmp
 
-def part(arr, l, r):
+
+def partition(arr, l, r):
+    # Uncoment next line if you would like to choose mid element as a pivot
+    # swap(arr, l + (r - l) // 2, r)
     p = r
     while True:
-        while arr[l] <= arr[p] and l < r:
+        while l < r and arr[l] <= arr[p]:
             l += 1
-        while arr[r] >= arr[p] and l < r:
+        while l < r and arr[r] >= arr[p]:
             r -= 1
         if l == r:
             break
@@ -16,10 +19,8 @@ def part(arr, l, r):
     swap(arr, r, p)
     return r
 
-            
-            
 def quickSort(arr, l, r):
     if l < r:
-        p = part(arr, l, r)
+        p = partition(arr, l, r)
         quickSort(arr, l, p - 1)
         quickSort(arr, p + 1, r)
