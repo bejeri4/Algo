f021b9f4229f19614a0a056bbb313ca5d2a53729
bejeri4 <lkara15@freeuni.edu.ge>
2020-11-24 19:09:34 +0400

    Update and rename PrimeNumbersCieve.py to PrimeNumbersSieve.py

--- PrimeNumbersSieve.py
+++ PrimeNumbersSieve.py
@@ -1,20 +1,22 @@
+# https://www.interviewbit.com/problems/prime-numbers/
+
 class Solution:
     # @param A : integer
     # @return a list of integers
     def sieve(self, A):
-        cieve = [1] * (A + 1)
-        cieve[0] = 0
-        cieve[1] = 0
+        sieve = [1] * (A + 1)
+        sieve[0] = 0
+        sieve[1] = 0
         i = 2
         while i <= A:
-            if cieve[i] == 1:
+            if sieve[i] == 1:
                 j = i + i
                 while j <= A:
-                    cieve[j] = 0
+                    sieve[j] = 0
                     j += i
             i += 1
         result = []
         for i in range(2, A + 1):
-            if cieve[i] == 1:
+            if sieve[i] == 1:
                 result.append(i)
         return result
