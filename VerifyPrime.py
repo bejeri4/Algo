78ec1f4a880d063fbdb6614ed64504983c85d28b
bejeri4 <lkara15@freeuni.edu.ge>
2020-11-24 19:12:19 +0400

    Create VerifyPrime.py

---
+++ VerifyPrime.py
@@ -0,0 +1,12 @@
+# https://www.interviewbit.com/problems/verify-prime/
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def isPrime(self, A):
+        if A == 1:
+            return 0
+        for i in range(2, int(math.sqrt(A)) + 1):
+            if A % i == 0:
+                return 0
+        return 1
