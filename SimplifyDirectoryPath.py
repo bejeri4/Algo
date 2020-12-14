0c8ad5a47a018007647791e1b1df5eafdd918334
bejeri4 <lkara15@freeuni.edu.ge>
2020-12-15 01:56:48 +0400

    Update SimplifyDirectoryPath.py

--- SimplifyDirectoryPath.py
+++ SimplifyDirectoryPath.py
@@ -1,1 +1,23 @@
 
+# https://www.interviewbit.com/problems/simplify-directory-path/
+
+class Solution:
+    # @param A : string
+    # @return a strings
+    def simplifyPath(self, A):
+        ls = A.split('/')
+        stack = []
+        for elem in ls:
+            if elem == "." or elem == "":
+                continue
+            elif elem == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(elem)
+        result = ""
+        while stack:
+            result += stack.pop()[::-1] + "/"
+        if result == "":
+            return "/"
+        return result[::-1]
