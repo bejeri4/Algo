b17536db86689c38d4d6632f88411b7ea50082eb
bejeri4 <lkara15@freeuni.edu.ge>
2021-02-15 15:06:03 +0400

    Update LCS.py

--- DP/LCS.py
+++ DP/LCS.py
@@ -13,9 +13,9 @@ for i in range(len(a) - 1, -1, -1):
         diag = ""
         if i <= len(a) - 2 and j <= len(b) - 2:
             diag = dp[i + 1][j + 1]
-        if i <= len(a) - 2:
-            horiz = dp[i + 1][j]
         if j <= len(b) - 2:
+            horiz = dp[i + 1][j]
+        if i <= len(a) - 2:
             vert = dp[i][j + 1]
         if a[i] == b[j]:
             diag = a[i] + diag
