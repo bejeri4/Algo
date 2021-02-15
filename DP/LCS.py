8060c3d62c8632646603ace2b485e9dbbf7a80a2
bejeri4 <lkara15@freeuni.edu.ge>
2021-02-15 15:07:04 +0400

    Update LCS.py

--- DP/LCS.py
+++ DP/LCS.py
@@ -14,9 +14,9 @@ for i in range(len(a) - 1, -1, -1):
         if i <= len(a) - 2 and j <= len(b) - 2:
             diag = dp[i + 1][j + 1]
         if j <= len(b) - 2:
-            horiz = dp[i + 1][j]
+            horiz = dp[i][j + 1]
         if i <= len(a) - 2:
-            vert = dp[i][j + 1]
+            vert = dp[i + 1][j]
         if a[i] == b[j]:
             diag = a[i] + diag
         maxLen = max(len(horiz), len(vert), len(diag))
