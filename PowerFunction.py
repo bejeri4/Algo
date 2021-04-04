a7e9511bf217f9384e9856d5e9f9cc27bc069e00
bejeri4 <lkara15@freeuni.edu.ge>
2021-04-04 12:31:29 +0400

    Update PowerFunction.py

--- PowerFunction.py
+++ PowerFunction.py
@@ -22,8 +22,6 @@ class Solution:
 def power(x, n, d):
     if n == 0:
         return 1
-    if n == 1:
-        return x
     tmp = power(x, n // 2, d)
     result = (tmp * tmp) % d
     if n % 2 == 1:
