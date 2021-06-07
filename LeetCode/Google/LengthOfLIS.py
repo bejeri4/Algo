9aa286d68ce0bf43f3ef6ebf0d61d40e62c94c17
bejeri4 <lkara15@freeuni.edu.ge>
2021-06-07 20:41:56 +0400

    Update LengthOfLIS.py

--- LeetCode/Google/LengthOfLIS.py
+++ LeetCode/Google/LengthOfLIS.py
@@ -1,3 +1,4 @@
+# O(n ^ 2)
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1] * len(nums)
@@ -6,4 +7,18 @@ class Solution:
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
+
+
+# O(n * log(n))
+class Solution1:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # dp = [1] * len(nums)
+        dp = []
+        for i in nums:
+            index = bisect_left(dp, i)
+            if index == len(dp):
+                dp.append(i)
+            else:
+                dp[index] = i
+        return len(dp)
 
